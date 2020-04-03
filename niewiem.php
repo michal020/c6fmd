929bd3c167d66022c94377e98447a40972d8253c
michal020 <ziom12m@gmail.com>
2020-04-03 19:35:07 +0200

    Update niewiem.php

--- niewiem.php
+++ niewiem.php
@@ -5,14 +5,6 @@
 damian pedał
 spierdalaj damian chuj
 luje
-sasasasasasasasadsa
-<label for="dsadasdas"></label>
-</html>
-=======
-dsadsayyyyy
-</html>
->>>>>>> d3984198f60c6da617aa652efa7ada15bdc161a2
-=======
-dsadsayyyyy
-</html>
->>>>>>> d3984198f60c6da617aa652efa7ada15bdc161a2
+
+<html>
+<h2>
